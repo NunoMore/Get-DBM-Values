1daa64038158544abfc68306485318caa3896f09
nuno moreira <nmore.professional@gmail.com>
2020-07-06 23:33:27 +0200

    some changes

--- Form1.cs
+++ Form1.cs
@@ -18,10 +18,10 @@ namespace GetDbmData
         private Uri uri = new Uri("http://websdr.ewi.utwente.nl:8901/"); // utwente university url by default
         private Uri uriTemp;
         
-        private static readonly string firstLineHeader = "\"DateStamp\";\"TimeStamp\";\"Frequency\";\"dbmValue\";\"dbmPeak\"";
+        private static readonly string firstLineHeader = "\"DateStamp\"; \"TimeStamp\"; \"Frequency\"; \"dbmValue\"; \"dbmPeak\"";
         private static readonly string pathSeparator = "\\";
         private static readonly string dateFormat = "dd-MM-yyy";
-        private static readonly char csvSeparatorSemiColon = ';';
+        private static readonly string csvSeparatorSemiColon = "; ";
         public static readonly string defaultFolderPath = Directory.GetCurrentDirectory() + pathSeparator + "csv_files" + pathSeparator + DateTime.Today.ToString(dateFormat);
 
         public Form1()
